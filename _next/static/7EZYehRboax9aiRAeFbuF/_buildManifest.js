e7595d270667d8e8068c3e5f6db95941c9e919e0
nsptel <nilkumarpatel48@gmail.com>
2022-03-06 14:33:26 -0500

    Add out directory

---
+++ _next/static/7EZYehRboax9aiRAeFbuF/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/1d63be8e50ea831a.css","static/chunks/pages/index-ae101f29b001161d.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

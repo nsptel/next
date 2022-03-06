f7c442b76e65cab36e44fc68b7f61abbf1e7a68a
nsptel <nilkumarpatel48@gmail.com>
2022-03-06 14:24:20 -0500

    Initial commit - hello world

---
+++ pages/_app.js
@@ -0,0 +1,5 @@
+import styles from "../styles/app.scss";
+
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import styles from "../styles/index.module.scss";
+
+export default function HomePage() {
+  return (
+    <div className={styles.headerContainer}>
+      <p>Hi</p>
+      <br />
+      <span>It's working</span>
+    </div>
+  );
+}
